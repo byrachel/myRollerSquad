55b6fbc330d166f90ac074fe2bfb9e2a83a16c98
Rachel NETHING <rachelnething@rachelnething.home>
2023-03-28 21:43:51 +0200

    wip flow slider

--- app/components/flow/getPosts/PicturesSlider.tsx
+++ app/components/flow/getPosts/PicturesSlider.tsx
@@ -6,17 +6,35 @@ interface Props {
 }
 
 export default function PicturesSlider({ urlPicts }: Props) {
+  const [currentPict, setCurrentPict] = React.useState(0);
+
   console.log(urlPicts);
   return (
     <div className="slider">
-      {urlPicts.map(urlPict => (
-        <Image
-          src={`https://myrollersquadflow.s3.eu-west-3.amazonaws.com/${urlPict}`}
-          alt="Roller Skateur"
-          className="sliderPict"
-          fill
-        />
-      ))}
+      <Image
+        src={`https://myrollersquadflow.s3.eu-west-3.amazonaws.com/${urlPicts[currentPict]}`}
+        alt="Roller Skateur"
+        className="sliderPict"
+        fill
+      />
+      <button
+        onClick={() =>
+          setCurrentPict(
+            currentPict === 0 ? urlPicts.length - 1 : currentPict - 1
+          )
+        }
+      >
+        Previous
+      </button>
+      <button
+        onClick={() =>
+          setCurrentPict(
+            currentPict === urlPicts.length - 1 ? 0 : currentPict + 1
+          )
+        }
+      >
+        Next
+      </button>
     </div>
   );
 }
