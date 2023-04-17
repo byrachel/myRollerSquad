0bb4b7c0947ecd9570774a10d34fa159a996a3ed
Rachel NETHING <rachelnething@rachelnething.home>
2023-04-17 22:00:56 +0200

    wip single post

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useMemo } from "react";
 import prisma from "../../server/prisma/db/client";
 import NewPostBar from "@/components/layouts/NewPostBar";
 import { PostInterface } from "app/interfaces/flowInterfaces";
+import { getCategoryName } from "app/constants/PostCategories";
+import { cardColor } from "app/utils/colorManager";
+import { getStyleName } from "app/constants/RollerSkateStyles";
+import LikeIcon from "@/components/flow/getPosts/LikeIcon";
+import { parseContent } from "app/utils/parseContent";
+import { displayLightDateTime } from "app/utils/handleDates";
+import Pin from "app/svg/pin.svg";
+import Avatar from "@/components/flow/getPosts/Avatar";
 
 interface Props {
   post: PostInterface;
@@ -9,11 +17,54 @@ interface Props {
 
 export default function Post({ post }: Props) {
   console.log(post);
+  const color = useMemo(() => cardColor(post.category_id), [post.category_id]);
+
   return (
     <>
       <NewPostBar />
-      <div>
-        <h1>{post.title}</h1>
+      <div className="sidebarLayout">
+        <div className="sidebarContent">
+          <Avatar userId={post.user.id} userAvatar={post.user.avatar} />
+
+          <div className="sidebarText">
+            <div className={`badge ${color}`}>
+              {getCategoryName(post.category_id)}
+            </div>
+            {post.style_id ? (
+              <div className={`outlineBadge ${color}`}>
+                {getStyleName(post.style_id)}
+              </div>
+            ) : null}
+            <div className="cardIcons">
+              <div className="cardSocialIcons">
+                <LikeIcon
+                  color={color}
+                  counter={post.user_likes.length}
+                  postId={post.id}
+                  likedBy={post.user_likes.map(like => like.user_id)}
+                />
+              </div>
+            </div>
+            <p className="meta">
+              myRollerSquad est une communauté active & bienveillante de
+              passionnés de roller quad.
+            </p>
+          </div>
+        </div>
+        <div className="sidebarContainer">
+          <h2>{post.title}</h2>
+          <div className="singleCardMeta">
+            <p className="cardMetaText">
+              {displayLightDateTime(post.created_at)}
+              <Pin width={12} height={12} className="metaIcon" />
+              {post.city ? `${post.city}, ` : null}
+              {post.country}
+            </p>
+          </div>
+          <div className="singlePostContent">
+            {post.content ? parseContent(post.content) : null}
+          </div>
+        </div>
       </div>
     </>
   );
@@ -48,13 +99,18 @@ export async function getStaticProps({ params: { id } }: IParams) {
     include: {
       comments: true,
       user_likes: true,
+      user: {
+        select: {
+          id: true,
+        },
+      },
     },
   });
 
   const data = {
     ...post,
     created_at: post ? post.created_at.toISOString() : null,
-    user_likes: post ? post.user_likes.length : [],
+    // user_likes: post ? post.user_likes.length : [],
   };
 
   return {
