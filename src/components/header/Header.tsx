70e8053767ff7af6bd4a3b29047d2080f0e578f7
Rachel Nething <r.nething@e4-ia.fr>
2023-05-18 16:23:49 +0200

    wip responsive nav with avatar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
-import { Dropdown } from "@nextui-org/react";
+import { Avatar, Dropdown, Navbar, Text } from "@nextui-org/react";
 import { onLogout } from "../../features/auth/utils/services";
 import { State, useUser } from "src/hooks/useUser";
 import { shallow } from "zustand/shallow";
@@ -32,93 +32,178 @@ export default function Header() {
   const isLogged = isLoggedIn && userId;
   const isAdmin = isLoggedIn && userRole === "ADMIN";
 
+  const goTo = (actionKey: string) => {
+    switch (actionKey) {
+      case "myProfile":
+        router.push(`/profile/me`);
+        break;
+      case "myFavs":
+        router.push(`/profile/favs/${userId}`);
+        break;
+      case "myPosts":
+        router.push(`/profile/posts/${userId}`);
+        break;
+      case "logout":
+        onLogout(userLogout, router);
+        break;
+      default:
+        router.push("/");
+    }
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <Link href="/">
-          <Image
-            className={styles.logo}
-            src="/logo_myrollersquad_web.png"
-            alt="Logo My Roller Squad"
-            width="240"
-            height="78"
-            priority
-          />
-        </Link>
-        <div className={styles.navigation}>
-          <div className={styles.navigationIcon}>
-            <Link href={"/business/search/all/all"}>
-              <p className={path === "business" ? styles.active : ""}>
-                Annuaire
-              </p>
-            </Link>
-            <Link href={"/myrollerblog"}>
-              <p className={path === "myrollerblog" ? styles.active : ""}>
-                Flow
-              </p>
-            </Link>
+    <Navbar isBordered variant="sticky">
+      <Navbar.Toggle showIn="xs" />
+      <Navbar.Brand>
+        <Image
+          className={styles.logo}
+          src="/logo_myrollersquad_web.png"
+          alt="Logo My Roller Squad"
+          width="240"
+          height="78"
+          priority
+        />
+      </Navbar.Brand>
+      <Navbar.Content
+        enableCursorHighlight
+        activeColor="secondary"
+        hideIn="xs"
+        // variant="underline"
+        variant="highlight-rounded"
+      >
+        <Navbar.Link
+          isActive={path === "business"}
+          href="/business/search/all/all"
+        >
+          Annuaire
+        </Navbar.Link>
+        <Navbar.Link isActive={path === "myrollerblog"} href="/myrollerblog">
+          Flow
+        </Navbar.Link>
+      </Navbar.Content>
 
-            {isLogged ? (
-              <Dropdown>
-                <Dropdown.Button color="secondary" light>
-                  <UserProfile
-                    className={
-                      myProfilePage ? styles.icon : styles.iconInactive
-                    }
-                    width={38}
-                    height={38}
-                  />
-                </Dropdown.Button>
-                <Dropdown.Menu aria-label="Static Actions">
-                  <Dropdown.Item>
-                    <Link href={"/profile/me"}>Mon compte</Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <Link href={`/profile/posts/${userId}`}>
-                      Mes publications
-                    </Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <Link href={`/profile/favs/${userId}`}>Mes favoris</Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item withDivider color="error">
-                    <span
-                      role="button"
-                      tabIndex={0}
-                      onKeyDown={() => onLogout(userLogout, router)}
-                      onClick={() => onLogout(userLogout, router)}
-                    >
-                      Se déconnecter
-                    </span>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            ) : (
-              <Link href={"/signin"}>
-                <UserProfile
-                  className={
-                    path === "profile" ? styles.icon : styles.iconInactive
-                  }
-                  width={38}
-                  height={38}
-                />
-              </Link>
-            )}
+      <Navbar.Content
+        css={{
+          "@xs": {
+            w: "12%",
+            jc: "flex-end",
+          },
+        }}
+      >
+        <Dropdown placement="bottom-right">
+          <Navbar.Item>
+            <Dropdown.Trigger>
+              <Avatar
+                bordered
+                as="button"
+                color="secondary"
+                size="md"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </Dropdown.Trigger>
+          </Navbar.Item>
+          <Dropdown.Menu
+            aria-label="User menu actions"
+            color="secondary"
+            onAction={(actionKey: any) => goTo(actionKey)}
+          >
+            <Dropdown.Item key="myProfile">Mon compte</Dropdown.Item>
+            <Dropdown.Item key="myPosts">Mes publications</Dropdown.Item>
+            <Dropdown.Item key="myFavs">Mes favoris</Dropdown.Item>
+            <Dropdown.Item key="logout" withDivider color="error">
+              Se déconnecter
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar.Content>
+    </Navbar>
 
-            {isAdmin ? (
-              <Link href={isLogged ? "/board/manager" : "/signin"}>
-                <Admin
-                  className={
-                    path === "board" ? styles.icon : styles.iconInactive
-                  }
-                  width={38}
-                  height={38}
-                />
-              </Link>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </header>
+    // <header className={styles.header}>
+    //   <div className={styles.container}>
+    //     <Link href="/">
+    //       <Image
+    //         className={styles.logo}
+    //         src="/logo_myrollersquad_web.png"
+    //         alt="Logo My Roller Squad"
+    //         width="240"
+    //         height="78"
+    //         priority
+    //       />
+    //     </Link>
+    //     <div className={styles.navigation}>
+    //       <div className={styles.navigationIcon}>
+    //         <Link href={"/business/search/all/all"}>
+    //           <p className={path === "business" ? styles.active : ""}>
+    //             Annuaire
+    //           </p>
+    //         </Link>
+    //         <Link href={"/myrollerblog"}>
+    //           <p className={path === "myrollerblog" ? styles.active : ""}>
+    //             Flow
+    //           </p>
+    //         </Link>
+
+    //         {isLogged ? (
+    //           <Dropdown>
+    //             <Dropdown.Button color="secondary" light>
+    //               <UserProfile
+    //                 className={
+    //                   myProfilePage ? styles.icon : styles.iconInactive
+    //                 }
+    //                 width={38}
+    //                 height={38}
+    //               />
+    //             </Dropdown.Button>
+    //             <Dropdown.Menu aria-label="Static Actions">
+    //               <Dropdown.Item>
+    //                 <Link href={"/profile/me"}>Mon compte</Link>
+    //               </Dropdown.Item>
+    //               <Dropdown.Item>
+    //                 <Link href={`/profile/posts/${userId}`}>
+    //                   Mes publications
+    //                 </Link>
+    //               </Dropdown.Item>
+    //               <Dropdown.Item>
+    //                 <Link href={`/profile/favs/${userId}`}>Mes favoris</Link>
+    //               </Dropdown.Item>
+    //               <Dropdown.Item withDivider color="error">
+    //                 <span
+    //                   role="button"
+    //                   tabIndex={0}
+    //                   onKeyDown={() => onLogout(userLogout, router)}
+    //                   onClick={() => onLogout(userLogout, router)}
+    //                 >
+    //                   Se déconnecter
+    //                 </span>
+    //               </Dropdown.Item>
+    //             </Dropdown.Menu>
+    //           </Dropdown>
+    //         ) : (
+    //           <Link href={"/signin"}>
+    //             <UserProfile
+    //               className={
+    //                 path === "profile" ? styles.icon : styles.iconInactive
+    //               }
+    //               width={38}
+    //               height={38}
+    //             />
+    //           </Link>
+    //         )}
+
+    //         {isAdmin ? (
+    //           <Link href={isLogged ? "/board/manager" : "/signin"}>
+    //             <Admin
+    //               className={
+    //                 path === "board" ? styles.icon : styles.iconInactive
+    //               }
+    //               width={38}
+    //               height={38}
+    //             />
+    //           </Link>
+    //         ) : null}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </header>
   );
 }
