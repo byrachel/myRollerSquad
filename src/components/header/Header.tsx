be3798e21928a338708f4007c025045593fbe0a5
Rachel Nething <r.nething@e4-ia.fr>
2023-05-18 22:26:35 +0200

    responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,23 @@
+import { useRef, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
-import { Avatar, Dropdown, Navbar, Text } from "@nextui-org/react";
+import { Avatar, Button, Dropdown, Navbar, Text } from "@nextui-org/react";
 import { onLogout } from "../../features/auth/utils/services";
 import { State, useUser } from "src/hooks/useUser";
 import { shallow } from "zustand/shallow";
 
 import styles from "../../styles/Header.module.scss";
 
-import UserProfile from "src/svg/profile-circle.svg";
-import Admin from "src/svg/admin.svg";
+// import UserProfile from "src/svg/profile-circle.svg";
 // import MySquad from "src/svg/flash.svg";
 // import Search from "src/svg/search.svg";
 
 export default function Header() {
   const router = useRouter();
+  const navbarToggleRef = useRef() as any;
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState<any>(false);
+
   const path = router.pathname.split("/")[1];
   const myProfilePage =
     router.pathname === "/profile/[uid]" && router.query.uid === "me";
@@ -31,6 +34,21 @@ export default function Header() {
 
   const isLogged = isLoggedIn && userId;
   const isAdmin = isLoggedIn && userRole === "ADMIN";
+
+  const collapseItems = [
+    { id: 1, name: "Annuaire", link: "/business/search/all/all" },
+    { id: 2, name: "Blog", link: "/myrollerblog" },
+  ];
+
+  const adminItems = [
+    ...collapseItems,
+    { id: 4, name: "Admin", link: "/board/manager" },
+  ];
+
+  const handleSideMenu = (link: string) => {
+    router.push(link);
+    isSideMenuOpen && navbarToggleRef.current.click();
+  };
 
   const goTo = (actionKey: string) => {
     switch (actionKey) {
@@ -53,7 +71,11 @@ export default function Header() {
 
   return (
     <Navbar isBordered variant="sticky">
-      <Navbar.Toggle showIn="xs" />
+      <Navbar.Toggle
+        showIn="xs"
+        ref={navbarToggleRef}
+        onChange={(isSelected) => setIsSideMenuOpen(isSelected)}
+      />
       <Navbar.Brand>
         <Image
           className={styles.logo}
@@ -66,10 +88,9 @@ export default function Header() {
       </Navbar.Brand>
       <Navbar.Content
         enableCursorHighlight
-        activeColor="secondary"
+        activeColor="error"
         hideIn="xs"
-        // variant="underline"
-        variant="highlight-rounded"
+        variant="highlight"
       >
         <Navbar.Link
           isActive={path === "business"}
@@ -78,8 +99,13 @@ export default function Header() {
           Annuaire
         </Navbar.Link>
         <Navbar.Link isActive={path === "myrollerblog"} href="/myrollerblog">
-          Flow
+          Blog
         </Navbar.Link>
+        {isAdmin ? (
+          <Navbar.Link href="/board/manager">
+            <Text weight="bold">Admin</Text>
+          </Navbar.Link>
+        ) : null}
       </Navbar.Content>
 
       <Navbar.Content
@@ -90,32 +116,68 @@ export default function Header() {
           },
         }}
       >
-        <Dropdown placement="bottom-right">
+        {isLogged ? (
+          <Dropdown placement="bottom-right">
+            <Navbar.Item>
+              <Dropdown.Trigger>
+                <Avatar
+                  bordered={myProfilePage}
+                  as="button"
+                  color="secondary"
+                  size="lg"
+                  src="/img/myrollersquad_avatar.jpeg"
+                />
+              </Dropdown.Trigger>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="User menu actions"
+              color="secondary"
+              onAction={(actionKey: any) => goTo(actionKey)}
+            >
+              <Dropdown.Item key="myProfile">Mon compte</Dropdown.Item>
+              <Dropdown.Item key="myPosts">Mes publications</Dropdown.Item>
+              <Dropdown.Item key="myFavs">Mes favoris</Dropdown.Item>
+              <Dropdown.Item key="logout" withDivider color="error">
+                Se déconnecter
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
           <Navbar.Item>
-            <Dropdown.Trigger>
-              <Avatar
-                bordered
-                as="button"
-                color="secondary"
-                size="md"
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-              />
-            </Dropdown.Trigger>
+            <Button auto flat as={Link} color="error" href="/signin">
+              Se connecter
+            </Button>
           </Navbar.Item>
-          <Dropdown.Menu
-            aria-label="User menu actions"
-            color="secondary"
-            onAction={(actionKey: any) => goTo(actionKey)}
-          >
-            <Dropdown.Item key="myProfile">Mon compte</Dropdown.Item>
-            <Dropdown.Item key="myPosts">Mes publications</Dropdown.Item>
-            <Dropdown.Item key="myFavs">Mes favoris</Dropdown.Item>
-            <Dropdown.Item key="logout" withDivider color="error">
-              Se déconnecter
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        )}
       </Navbar.Content>
+      <Navbar.Collapse>
+        {isAdmin
+          ? adminItems.map((item) => (
+              <Navbar.CollapseItem key={item.id}>
+                <Text
+                  color="inherit"
+                  weight="bold"
+                  onClick={() => handleSideMenu(item.link)}
+                >
+                  {item.name}
+                </Text>
+              </Navbar.CollapseItem>
+            ))
+          : collapseItems.map((item) => (
+              <Navbar.CollapseItem key={item.id}>
+                <Text
+                  color="inherit"
+                  weight="bold"
+                  css={{
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleSideMenu(item.link)}
+                >
+                  {item.name}
+                </Text>
+              </Navbar.CollapseItem>
+            ))}
+      </Navbar.Collapse>
     </Navbar>
 
     // <header className={styles.header}>
