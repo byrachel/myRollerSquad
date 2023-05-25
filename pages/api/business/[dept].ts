9dba9ec9ac9ed49f1bb54554fdfca6b4989d05e1
Rachel Nething <r.nething@e4-ia.fr>
2023-05-25 15:47:14 +0200

    🚧 nextAuth

--- pages/api/business/[dept].ts
+++ pages/api/business/[dept].ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponse } from "next";
 import nextConnect from "next-connect";
 
 import prisma from "server/prisma/db/client";
@@ -6,37 +6,35 @@ import { E1 } from "src/constants/ErrorMessages";
 
 const handler = nextConnect();
 
-export default handler.get(
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    const { dept, category } = req.query;
+export default handler.get(async (req: any, res: NextApiResponse) => {
+  const { dept, category } = req.query;
 
-    try {
-      const places = await prisma.place.findMany({
-        take: 100,
-        orderBy: {
-          created_at: "desc",
-        },
-        where: {
-          active: true,
-          ...(!dept || dept === "all" ? {} : { county: dept }),
-          ...(!category || category === "all" ? {} : { category }),
-        },
-        include: {
-          favorites: {
-            select: {
-              id: true,
-            },
-          },
-          posts: {
-            select: {
-              id: true,
-            },
+  try {
+    const places = await prisma.place.findMany({
+      take: 100,
+      orderBy: {
+        created_at: "desc",
+      },
+      where: {
+        active: true,
+        ...(!dept || dept === "all" ? {} : { county: dept }),
+        ...(!category || category === "all" ? {} : { category }),
+      },
+      include: {
+        favorites: {
+          select: {
+            id: true,
           },
         },
-      });
-      res.status(200).json({ places });
-    } catch (e) {
-      res.status(400).json({ message: E1 });
-    }
+        posts: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    res.status(200).json({ places });
+  } catch (e) {
+    res.status(400).json({ message: E1 });
   }
-);
+});
