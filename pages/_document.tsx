5b0ed5966a3971332871462115646f8bb657c9af
Rachel NETHING <rachelnething@rachelnething.home>
2023-05-31 19:17:52 +0200

    wip pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
       <Head>
         {CssBaseline.flush()}
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
+        {/* <link rel="apple-touch-icon" href="/icon.png"></link> */}
         <meta name="theme-color" content="#fff" />
       </Head>
       <body>
